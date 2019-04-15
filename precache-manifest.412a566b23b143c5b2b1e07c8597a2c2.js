c7da77b9d9710d5008a4a432d1882460c3674b26
Philip <philiplofters@gmail.com>
2019-04-15 15:59:15 -0400

    Updates

---
+++ precache-manifest.412a566b23b143c5b2b1e07c8597a2c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboBuddies/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "55b638fe4750958bb782",
+    "url": "/RoboBuddies/static/js/runtime~main.f059b7f3.js"
+  },
+  {
+    "revision": "b3b441896b60f161e64f",
+    "url": "/RoboBuddies/static/js/main.1c5af192.chunk.js"
+  },
+  {
+    "revision": "dffc6ed8a14b2aa3d65d",
+    "url": "/RoboBuddies/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "b3b441896b60f161e64f",
+    "url": "/RoboBuddies/static/css/main.6bf52a6c.chunk.css"
+  },
+  {
+    "revision": "dffc6ed8a14b2aa3d65d",
+    "url": "/RoboBuddies/static/css/2.a418723e.chunk.css"
+  },
+  {
+    "revision": "f6591a0c45718d760b3254fcaf7394c0",
+    "url": "/RoboBuddies/index.html"
+  }
+];
